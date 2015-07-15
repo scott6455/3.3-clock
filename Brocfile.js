23e8ec6d96d40c3207eea2cd94f2ce237f234602
scott6455 <scottgreen555@gmail.com>
2015-07-15 14:14:11 -0500

    initial commit for 3.3 project start

---
+++ Brocfile.js
@@ -0,0 +1,14 @@
+var merge = require('broccoli-merge-trees');
+var sass = require('broccoli-sass');
+
+var includePaths = [
+    'assets/scss',
+    'bower_components/reset-css',
+    'bower_components/bourbon/app/assets/stylesheets',
+
+];
+
+var compiledCss = sass ('includePaths', 'app.scss', 'app.css');
+
+module.exports = merge(['public', 'assets/js', compiledCss]);
+
